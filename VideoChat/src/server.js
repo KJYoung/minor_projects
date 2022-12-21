6d02cdbcedb429bdac462578d759faf3788d06e7
KJYoung <jykim157@snu.ac.kr>
2022-12-22 01:36:53 +0900

    [VideoChat][Setting] Frontend Setup

--- VideoChat/src/server.js
+++ VideoChat/src/server.js
@@ -4,4 +4,10 @@ const app = express();
 
 console.log("Project Initialized");
 
-app.listen(3000);
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render("home"));
+// Listen
+app.listen(3000, () => console.log(`Listening on http://localhost:3000`));
