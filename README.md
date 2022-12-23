3b396d65d37b2b205c51dca565b321fd20f23816
KJYoung <jykim157@snu.ac.kr>
2022-12-23 23:47:10 +0900

    [VideoChat][Setting] Redirect

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## 작은 규모의 개발 관련 프로젝트들을 위한 Repository입니다.
 각각을 위한 repository를 따로 생성하기에는 너무 지저분할 것 같아 생성해둡니다.
+
+- [VideoChat(WebRTC와 WebSocket을 이용한 화상통화 구현)](https://github.com/KJYoung/minor_projects/tree/main/VideoChat)
--- VideoChat/src/server.js
+++ VideoChat/src/server.js
@@ -9,5 +9,6 @@ app.set('views', __dirname + '/views');
 
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render("home"));
+app.get('/*', (req, res) => res.redirect('/'));
 // Listen
 app.listen(3000, () => console.log(`Listening on http://localhost:3000`));
