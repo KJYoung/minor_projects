7891c2358322b106789e70c50d7e9869e3506a73
KJYoung <jykim157@snu.ac.kr>
2022-12-24 23:41:06 +0900

    [VideoChat][Setting] WebSocket connection via ws

--- VideoChat/src/public/javascript/app.js
+++ VideoChat/src/public/javascript/app.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("CONNECTED to Server!");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("Just got this: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("DISCONNECTED to Server!");
+});
+
+setTimeout(() => {
+    socket.send("Hello from Browser!");
+}, 5000);
--- VideoChat/src/server.js
+++ VideoChat/src/server.js
@@ -1,8 +1,9 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
 
-console.log("Project Initialized");
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
@@ -11,4 +12,17 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render("home"));
 app.get('/*', (req, res) => res.redirect('/'));
 // Listen
-app.listen(3000, () => console.log(`Listening on http://localhost:3000`));
+
+const listenLogger = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+wss.on("connection", (socket) => {
+    socket.send("hello from Backend");
+    socket.on("close", () => console.log("Disconnected to Browser"));
+    socket.on("message", (message) => console.log(message.toString('utf8')));
+    console.log("Connected to Browser");
+});
+
+server.listen(3000, listenLogger);
