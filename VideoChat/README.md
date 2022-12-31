9116d6d30df7500c4eafd497fe27bd0b131bfc83
KJYoung <jykim157@snu.ac.kr>
2023-01-01 02:14:05 +0900

    [VideoChat][Setting] END

--- VideoChat/README.md
+++ VideoChat/README.md
@@ -1,3 +1,4 @@
 ## WebRTC와 WebSocket을 이용한 화상통화 구현
 
 - WebRTC는 peer의 수가 많으면 성능이 좋지 않다.
+- WebRTC의 upload workload를 줄이는 방법으로 SFU(Selective Forwarding Unit)을 쓸 수 있다.
--- VideoChat/src/public/javascript/video.js
+++ VideoChat/src/public/javascript/video.js
@@ -132,7 +132,6 @@ const makeConnection = () => {
         iceServers: [
             {
                 urls: [
-                    "stun:stun.l.google.com:19302",
                     "stun:stun1.l.google.com:19302",
                     "stun:stun2.l.google.com:19302",
                     "stun:stun3.l.google.com:19302",
