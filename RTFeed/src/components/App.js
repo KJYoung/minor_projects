15aff3bab87973ef3390b9f7aef545be9c7ff9ac
KJYoung <jykim157@snu.ac.kr>
2023-01-04 01:10:42 +0900

    [RTFeed][Setting] Firebase Auth, JS absolute import

--- RTFeed/src/components/App.js
+++ RTFeed/src/components/App.js
@@ -1,8 +1,14 @@
-import MyRouter from "./Router";
+import { useState } from "react";
+import MyRouter from "components/Router";
+import { authService } from "fbConfig";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
-    <MyRouter/>
+    <>
+      <MyRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; RTFeed. 2023 Jan.</footer>
+    </>
   );
 }
 
