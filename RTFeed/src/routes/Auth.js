aadc99f4db47e6af0f0be8801fab67c4acbb18a4
KJYoung <jykim157@snu.ac.kr>
2023-01-05 17:58:07 +0900

    [RTFeed][FEAT] Auth Form1

--- RTFeed/src/routes/Auth.js
+++ RTFeed/src/routes/Auth.js
@@ -1,4 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <div>Auth</div>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(false);
+  
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input name="email" type="text" placeholder="Email" required
+                   value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <input name="password" type="password" placeholder="Password" required
+                   value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <input type="submit" value="Log In" />
+        </form>
+        <div>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
+        </div>
+    </div>)
+  };
 export default Auth;
