ed3dffd98c52f337c8996466c1c7df059eb44b58
KJYoung <jykim157@snu.ac.kr>
2023-03-11 02:17:56 +0900

    [DartBasics] Nullable, Final Var

--- DartBasics/DartBasic1.dart
+++ DartBasics/DartBasic1.dart
@@ -1,6 +1,32 @@
 void main() {
-  // Variable.
+  // --- Variable.  --------------------------------------------
   var variable1 = "Hey! ";
   String variable2 = "Hello World!";
   print("Hello! " + variable1 + variable2);
+
+  var variable3; // dynamic type.
+  dynamic variable4; // dynamic type.
+  if (variable4 is String) {
+    print("Variable4 is String and its length is... " +
+        variable4.length.toString());
+  }
+  ;
+  // -- 1.3 About Nullable Variables.
+  bool isEmpty(String string) => string.length == 0;
+  // isEmpty(null); // Null can't be assigned to String type var.
+
+  String? variableCanbeNull = 'Str';
+  variableCanbeNull = null;
+
+  // variableCanbeNull.length; // ERROR! Could be null.
+  if (variableCanbeNull != null) {
+    variableCanbeNull.length; // Is not error. Null Checked.
+  }
+  ;
+  variableCanbeNull?.length; // OR Use '?'.
+
+  // -- 1.4 Final Variables.
+  final String varFin = 'varFin';
+  final varFin2 = "varFin2";
+  print("Final " + varFin + varFin2);
 }
