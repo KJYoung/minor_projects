10bbbb5187d9eff18ca5c8f0b2231a42a738f2ae
KJYoung <jykim157@snu.ac.kr>
2023-03-14 22:06:06 +0900

    [DartBasics] Ignore unused_local_var

---
+++ DartBasics/DartBasic2.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  // --- 2. Data Types.  --------------------------------------------
+  String name = 'VKJY';
+  int number = 3;
+  double number21 = 3;
+  double number22 = 3.4;
+  bool boolean = true;
+  num generalNumber1 = 3;
+  num generalNumber2 = 3.4;
+}
