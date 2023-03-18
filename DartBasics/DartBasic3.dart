a442b9f73cd61e65b15d78a5c7f6542a024dbc8c
KJYoung <jykim157@snu.ac.kr>
2023-03-19 02:41:36 +0900

    [DartBasics] Basic Functions.

--- DartBasics/DartBasic3.dart
+++ DartBasics/DartBasic3.dart
@@ -1,5 +1,21 @@
 // ignore_for_file: unused_local_variable
 
+void printFn(String content) {
+  print(content);
+}
+
+// Direct Return.
+String stringFormatter(String content) => "{{{ $content }}}";
+// Named Parameter.
+String bussinessFormatter({String name = "annonymous", String pos = "void"}) =>
+    "Hi, I am $name, $pos";
+String bussinessFormatter2({required String name, required String pos}) =>
+    "Hi, I am $name, $pos";
+
 void main() {
   // --- 3. Functions.  --------------------------------------------
+  printFn("Say Hello");
+  printFn(stringFormatter("SIU!"));
+  printFn(bussinessFormatter(name: 'hi', pos: '33'));
+  // printFn(bussinessFormatter2(name: 'hi')); // ERROR
 }
