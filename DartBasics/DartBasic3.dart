d9bfab18b045b3c1cdb134dd704c715eaef0123e
KJYoung <jykim157@snu.ac.kr>
2023-03-19 23:41:35 +0900

    [DartBasics] Parameter, QQ(??), Typedef.

--- DartBasics/DartBasic3.dart
+++ DartBasics/DartBasic3.dart
@@ -11,6 +11,33 @@ String bussinessFormatter({String name = "annonymous", String pos = "void"}) =>
     "Hi, I am $name, $pos";
 String bussinessFormatter2({required String name, required String pos}) =>
     "Hi, I am $name, $pos";
+// Optional Parameter.
+String bussinessFormatter3(String name, String pos, [String? addit]) =>
+    "Hi, I am $name, $pos + $addit";
+// QQ Operator.
+String capitalizeStr(String str) => str.toUpperCase();
+String capitalizeStrNULL(String? str) {
+  if (str != null) {
+    return str.toUpperCase();
+  } else {
+    return '';
+  }
+}
+
+String capitalizeStrNULL2(String? str) => str != null ? str.toUpperCase() : '';
+String capitalizeStrNULL3(String? str) => str?.toUpperCase() ?? '';
+
+// Typedef
+List<int> reverseListINT(List<int> lst) {
+  var reversed = lst.reversed;
+  return reversed.toList();
+}
+
+typedef IntList = List<int>;
+IntList reverseListINT2(IntList lst) {
+  var reversed = lst.reversed;
+  return reversed.toList();
+}
 
 void main() {
   // --- 3. Functions.  --------------------------------------------
@@ -18,4 +45,14 @@ void main() {
   printFn(stringFormatter("SIU!"));
   printFn(bussinessFormatter(name: 'hi', pos: '33'));
   // printFn(bussinessFormatter2(name: 'hi')); // ERROR
+  printFn(bussinessFormatter3('Messia', '23'));
+  printFn(bussinessFormatter3('Messia', '23', 'æ'));
+  printFn(capitalizeStr('junyoung kim'));
+
+  String? str1;
+  str1 ??= 'Hey'; // Assign if str1 is null.
+  print(str1);
+  str1 ??=
+      'Joseph'; // Assign if str1 is null. i.e. This line would have no effect.
+  print(str1);
 }
