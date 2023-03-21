0eb55c6ce1562a3961edd75b660eb7349e59429b
KJYoung <jykim157@snu.ac.kr>
2023-03-22 01:00:33 +0900

    [DartBasics] Class 2.

--- DartBasics/DartBasic4.dart
+++ DartBasics/DartBasic4.dart
@@ -20,8 +20,29 @@ class PlayerShortCut {
   PlayerShortCut(this.name);
 }
 
+class PlayerNamedParams {
+  String name;
+
+  // Named Params Constructor.
+  PlayerNamedParams({required this.name});
+}
+
+class PlayerNamedConst {
+  String name;
+  late int state;
+
+  // Named Constructor.
+  PlayerNamedConst({required this.name}) : this.state = 0;
+
+  PlayerNamedConst.createWithState({required name, required this.state})
+      : this.name = name;
+}
+
 void main() {
   // --- 4. Classes.  --------------------------------------------
   var p1 = Player('VKJY');
   p1.printIntro();
+
+  var p2 = PlayerNamedConst(name: 'jason');
+  var p3 = PlayerNamedConst.createWithState(name: 'juhe', state: 1);
 }
