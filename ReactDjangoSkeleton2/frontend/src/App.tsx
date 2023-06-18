120af59724e12f2a7637a5683a9d1c3b330ab0cf
KJYoung <jykim157@snu.ac.kr>
2023-06-18 15:41:53 +0900

    [ReactDjangoSkeleton2] Skeleton Complete. ReduxSaga Replaced

--- ReactDjangoSkeleton2/frontend/src/App.tsx
+++ ReactDjangoSkeleton2/frontend/src/App.tsx
@@ -1,32 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { ERRORSTATE, coreActions } from './store/slices/core';
-import { RootState } from '.';
+import { ERRORSTATE, createCore, deleteCore, editCore, fetchCores, selectCore } from './store/slices/core';
+import { AppDispatch } from './store';
 
 function App() {
   const [name, setName] = useState("");
   const [editName, setEditName] = useState("");
   const [editID, setEditID] = useState(-1);
 
-  const dispatch = useDispatch();
-  const { coreElements, coreState } = useSelector(({ core }: RootState) => ({ coreElements: core.elements, coreState: core.errorState }));
+  const dispatch = useDispatch<AppDispatch>();
+  const { elements, errorState }  = useSelector(selectCore);
 
   useEffect(() => {
-    if(coreState === ERRORSTATE.SUCCESS){
-      dispatch(coreActions.getElements());
+    if(errorState === ERRORSTATE.SUCCESS){
+      dispatch(fetchCores());
     }
-  }, [coreState, dispatch]);
+  }, [elements, errorState, dispatch]);
   return (
     <div className="App">
-      <button onClick={() => { dispatch(coreActions.getElements()); }}>FETCH!</button>
-      <input value={name} onChange={(e) => {setName(e.target.value)}}/><button disabled={name===""} onClick={() => { dispatch(coreActions.createCore({name})); setName(""); }}>SEND</button>
-      {coreElements && coreElements.map((e, index) => 
-        <div>
+      <button onClick={() => { dispatch(fetchCores()); }}>FETCH!</button>
+      <input value={name} onChange={(e) => {setName(e.target.value)}}/><button disabled={name===""} onClick={() => { dispatch(createCore(name)); setName(""); }}>SEND</button>
+      {elements && 
+        elements.map((e, index) => 
+        <div key={index}>
           {editID === e.id ? 
             <>
               <input value={editName} onChange={(e) => setEditName(e.target.value)}/>
               <button disabled={editName === ""} onClick={() => {
-                dispatch(coreActions.editCore({id: e.id, name: editName}));
+                dispatch(editCore({id: e.id, name: editName}));
                 setEditID(-1); setEditName("");
               }}>수정 완료</button>
             </>
@@ -34,9 +35,10 @@ function App() {
             <span>{e.id} | {e.name}</span>
           }
             {editID !== e.id && <button onClick={async () => { setEditID(e.id); setEditName(e.name)}}>수정</button>}
-            <button onClick={async () => dispatch(coreActions.deleteCore({id: e.id})) }>삭제</button>
+            <button onClick={async () => dispatch(deleteCore(e.id)) }>삭제</button>
         </div>
-      )}
+      )
+      }
     </div>
   );
 }
--- ReactDjangoSkeleton2/frontend/src/store/slices/core.ts
+++ ReactDjangoSkeleton2/frontend/src/store/slices/core.ts
@@ -25,20 +25,59 @@ export const initialState: CoreState = {
   errorState: ERRORSTATE.DEFAULT
 };
 
-export const fetchCore = createAsyncThunk(
-  "core/fetchCore",
+export const fetchCores = createAsyncThunk(
+  "core/fetchCores",
   async () => {
     const response = await client.get(`/api/core/`);
     return response.data;
   }
-)
+);
+export const createCore = createAsyncThunk(
+  "core/createCore",
+  async (coreName: String, { dispatch }) => {
+      const response = await client.post("/api/core/", {name : coreName});
+      dispatch(coreActions.createCore(response.data));
+  }
+);
+
+export const editCore = createAsyncThunk(
+  "core/editCore",
+  async (editCoreObj: CoreElement, { dispatch }) => {
+      const response = await client.put(`/api/core/${editCoreObj.id}/`, {name : editCoreObj.name});
+      dispatch(coreActions.editCore(response.data));
+  }
+);
+export const deleteCore = createAsyncThunk(
+  "core/deleteCore",
+  async (coreID: String | Number, { dispatch }) => {
+      const response = await client.delete(`/api/core/${coreID}/`);
+      dispatch(coreActions.deleteCore(response.data));
+  }
+);
 
 export const coreSlice = createSlice({
   name: "core",
   initialState,
   reducers: {
-    getElement: (state, action: PayloadAction<{}>) => {},
-  }
+    createCore: (state, action: PayloadAction<{}>) => {},
+    editCore: (state, action: PayloadAction<{}>) => {},
+    deleteCore: (state, action: PayloadAction<{}>) => {},
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchCores.fulfilled, (state, action) => {
+      state.elements = action.payload.elements;
+      state.errorState = ERRORSTATE.DEFAULT;
+    }); 
+    builder.addCase(createCore.fulfilled, (state, action) => {
+      state.errorState = ERRORSTATE.SUCCESS;
+    }); 
+    builder.addCase(editCore.fulfilled, (state, action) => {
+      state.errorState = ERRORSTATE.SUCCESS;
+    }); 
+    builder.addCase(deleteCore.fulfilled, (state, action) => {
+      state.errorState = ERRORSTATE.SUCCESS;
+    }); 
+  },
 });
 
 export const coreActions = coreSlice.actions;
