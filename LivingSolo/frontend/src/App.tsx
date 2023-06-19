e47788bc23e762924862a676800aeae67d943aaf
KJYoung <jykim157@snu.ac.kr>
2023-06-19 22:35:46 +0900

    [LivingSolo|Design] Introduce Material UI

--- LivingSolo/frontend/src/App.tsx
+++ LivingSolo/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ERRORSTATE, createCore, deleteCore, editCore, fetchCores, selectCore } from './store/slices/core';
 import { AppDispatch } from './store';
+import { Button } from '@mui/material';
 
 function App() {
   const [name, setName] = useState("");
@@ -18,24 +19,25 @@ function App() {
   }, [elements, errorState, dispatch]);
   return (
     <div className="App">
-      <button onClick={() => { dispatch(fetchCores()); }}>FETCH!</button>
-      <input value={name} onChange={(e) => {setName(e.target.value)}}/><button disabled={name===""} onClick={() => { dispatch(createCore(name)); setName(""); }}>SEND</button>
+      <Button variant={"contained"} onClick={() => { dispatch(fetchCores()); }}>FETCH!</Button>
+      <input value={name} onChange={(e) => {setName(e.target.value)}}/>
+      <Button variant={"contained"} disabled={name===""} onClick={() => { dispatch(createCore(name)); setName(""); }}>SEND</Button>
       {elements && 
         elements.map((e, index) => 
         <div key={index}>
           {editID === e.id ? 
             <>
               <input value={editName} onChange={(e) => setEditName(e.target.value)}/>
-              <button disabled={editName === ""} onClick={() => {
+              <Button variant={"contained"} disabled={editName === ""} onClick={() => {
                 dispatch(editCore({id: e.id, name: editName}));
                 setEditID(-1); setEditName("");
-              }}>수정 완료</button>
+              }}>수정 완료</Button>
             </>
             : 
             <span>{e.id} | {e.name}</span>
           }
-            {editID !== e.id && <button onClick={async () => { setEditID(e.id); setEditName(e.name)}}>수정</button>}
-            <button onClick={async () => dispatch(deleteCore(e.id)) }>삭제</button>
+            {editID !== e.id && <Button onClick={async () => { setEditID(e.id); setEditName(e.name)}} variant={"contained"}>수정</Button>}
+            <Button onClick={async () => dispatch(deleteCore(e.id))} variant={"contained"}>삭제</Button>
         </div>
       )
       }
--- LivingSolo/frontend/src/index.tsx
+++ LivingSolo/frontend/src/index.tsx
@@ -4,6 +4,11 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 document.getElementById('root')?.setAttribute('spellcheck', 'false');
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
