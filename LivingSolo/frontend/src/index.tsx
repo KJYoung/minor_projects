9b482be1c332cd2cdc54a0a17de24ed2fc634704
KJYoung <jykim157@snu.ac.kr>
2023-06-26 20:33:01 +0900

    [LivingSolo|Feat] Reset Styles

--- LivingSolo/frontend/src/index.tsx
+++ LivingSolo/frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import { store } from './store';
 import App from './App';
 
@@ -11,9 +13,60 @@ import '@fontsource/roboto/700.css';
 
 document.getElementById('root')?.setAttribute('spellcheck', 'false');
 
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+    outline: none;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    img {
+      -webkit-user-drag: none;
+    }
+    a {
+      -webkit-user-drag: none;
+    }
+    select {
+      -ms-user-select: none;
+      -moz-user-select: -moz-none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      user-select: none;
+    }
+    strong{
+      font-weight: bold;
+    }
+    em{
+      font-style: italic;
+    }
+  }
+  html {
+    height: 100%;
+  }
+  body {
+    box-sizing: border-box;
+    min-height: 100%;
+    line-height: 1;
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  #root {
+    min-height: 100%;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  } 
+`;
+
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <Provider store={store}>
+    <GlobalStyles />
     <App />
   </Provider>,
 );
