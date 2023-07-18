0153dfd28b106279038762f8bdfbc0d3ca73b56f
KJYoung <jykim157@snu.ac.kr>
2023-07-18 23:36:39 +0900

    [LivingSolo|Feat] Month Navigator FE - basic 1 month mover

--- LivingSolo/frontend/src/components/Trxn/TrxnGrid.tsx
+++ LivingSolo/frontend/src/components/Trxn/TrxnGrid.tsx
@@ -23,16 +23,72 @@ interface TrxnGridNavProps {
     viewMode: ViewMode,
     setViewMode: React.Dispatch<React.SetStateAction<ViewMode>>
 }
+
+type NavCalMonth = {
+    year: number;
+    month: number;
+};
+
 export function TrxnGridNav({viewMode, setViewMode} : TrxnGridNavProps) {
+    const [curMonth, setCurMonth] = useState<NavCalMonth>({
+        year: (new Date()).getFullYear(),
+        month: (new Date()).getMonth() + 1
+    });
+    const TrxnGridNavCalendar = () => {
+        const monthHandler = (am: number) => {
+            const afterMonth = curMonth.month + am;
+            if(afterMonth > 12)
+                setCurMonth({year: curMonth.year + Math.floor(afterMonth / 12), month: afterMonth % 12});
+            else if(afterMonth <= 0)
+                setCurMonth({year: curMonth.year - Math.floor(afterMonth / 12) - 1, month: 12 - ((-afterMonth) % 12)});
+            else
+                setCurMonth({...curMonth, month: afterMonth});
+        }
+        const MonthNavBtn = styled.button`
+          background-color: transparent;
+          border: none;
+          font-size: 25px;
+          color: var(--ls-gray_google2);
+          cursor: pointer;
+          &:hover {
+            background-color: var(--ls-gray_google1);
+          }
+          border-radius: 50%;
+          width: 38px;
+          height: 38px;
+          margin-left: 6px;
+          margin-right: 6px;
+        `;
+        const MonthIndSpan = styled.div`
+          font-size: 24px;
+          color: var(--ls-gray_google2);
+          width: 140px;
+          text-align: center;
+        `;
+        const MonthNavWrapper = styled.div`
+          width: 100%;
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+        `;
+        return <MonthNavWrapper>
+            <MonthNavBtn onClick={() => monthHandler(-1)}>{"<"}</MonthNavBtn>
+            <MonthIndSpan>{`${curMonth.year}년 ${curMonth.month}월`}</MonthIndSpan>
+            <MonthNavBtn onClick={() => monthHandler(+1)}>{">"}</MonthNavBtn>
+        </MonthNavWrapper>
+    }
+
     return <TrxnGridNavWrapper>
         <div>
             <TrxnGridModeBtn active={(viewMode === ViewMode.Detail).toString()} onClick={() => setViewMode(ViewMode.Detail)}>Detailed</TrxnGridModeBtn>
             <TrxnGridModeBtn active={(viewMode === ViewMode.Graph).toString()} onClick={() => setViewMode(ViewMode.Graph)}>Graphic</TrxnGridModeBtn>
         </div>
-        {viewMode === ViewMode.Detail && <div>
-            <button>{"<"}</button><span>2023.7</span><button>{">"}</button>
-            <input type="text" placeholder='검색'/><button>검색!</button>
-        </div>}
+        {viewMode === ViewMode.Detail && <TrxnGridDetailedSubNav>
+            <TrxnGridNavCalendar />
+            <div>
+                <input type="text" placeholder='검색'/><button>검색!</button>
+            </div>
+        </TrxnGridDetailedSubNav>}
     </TrxnGridNavWrapper>
 };
 
@@ -44,6 +100,13 @@ const TrxnGridNavWrapper = styled.div`
     align-items: center;
     
     margin-bottom: 15px;
+`;
+const TrxnGridDetailedSubNav = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 2fr 8fr;
+    padding-left: 70px;
+    padding-right: 70px;
 `;
 const TrxnGridModeBtn = styled.span<{ active: string }>`
     font-size: 22px;
