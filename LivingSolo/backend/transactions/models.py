c966f05e44dbe74abf94c17331a09f485c65fce4
KJYoung <jykim157@snu.ac.kr>
2023-07-21 00:37:06 +0900

    [LivingSolo|Feat|BE] Add PayMethod Model to Trxn

--- LivingSolo/backend/transactions/models.py
+++ LivingSolo/backend/transactions/models.py
@@ -6,6 +6,23 @@ from core.models import AbstractTimeStampedModel
 from tags.models import Tag
 from stockpiles.models import StockpileTransaction
 
+PAY_TYPE_CASH = "현금"
+PAY_TYPE_CHECK = "체크카드"
+PAY_TYPE_CREDIT = "신용카드"
+PAY_TYPE_PAY = "페이포인트"
+
+PAY_TYPE_CHOICES = (
+    (PAY_TYPE_CASH, "현금"),
+    (PAY_TYPE_CHECK, "체크카드"),
+    (PAY_TYPE_CREDIT, "신용카드"),
+    (PAY_TYPE_PAY, "페이포인트"),
+)
+
+
+class PayMethod(AbstractTimeStampedModel):
+    type = models.CharField(max_length=20, choices=PAY_TYPE_CHOICES, default=PAY_TYPE_CASH)
+    name = models.CharField(max_length=30, null=False)
+
 
 class Transaction(AbstractTimeStampedModel):
     """Transaction definition"""
@@ -20,6 +37,10 @@ class Transaction(AbstractTimeStampedModel):
     stockpile_transaction = models.ManyToManyField(
         StockpileTransaction, related_name="mother_trxn"
     )  # Related Stockpile Change
+
+    method = models.ForeignKey(
+        PayMethod, related_name='transaction', on_delete=models.SET_NULL, null=True
+    )
 
     class Meta:
         ordering = ("-date",)
