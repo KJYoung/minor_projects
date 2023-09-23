dac4b956accac14f7cc03f684e9095ce5b541a76
KJYoung <jykim157@snu.ac.kr>
2023-09-23 23:29:56 +0900

    [AI_SemanticSegmentation] 03_Train from Scratch

---
+++ AI_Study/SemanticSegmentation/U-Net_pytorch/train.py
@@ -0,0 +1,116 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import UNET
+
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_predictions_as_imags,
+)
+
+# HyperParameters
+LEARNING_RATE = 1e-4
+DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"  # torch 2.0.1
+# BATCH_SIZE = 32
+BATCH_SIZE = 16
+# NUM_EPOCHS = 100
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 160  # 1280 originally
+IMAGE_WIDTH = 240  # 1918 originally
+PIN_MEMORY = True
+# LOAD_MODEL = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "data/train_images/"
+TRAIN_MASK_DIR = "data/train_masks/"
+VAL_IMG_DIR = "data/val_images/"
+VAL_MASK_DIR = "data/val_masks/"
+
+# Train One Epoch
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device=DEVICE)
+        targets = (
+            targets.float().unsqueeze(1).to(device=DEVICE)
+        )  # unsqueeze를 통해 channel dimension 생성.
+
+        optimizer.zero_grad()
+
+        # forward
+        # torch.cuda.amp.autocast는 mps에서 불가능;;
+        predictions = model(data)
+        loss = loss_fn(predictions, targets)
+
+        # backward
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # update tqdm loop
+        loop.set_postfix(loss=loss.item())
+
+
+def main():
+    train_transform = A.Compose(
+        [
+            A.Resize(width=IMAGE_WIDTH, height=IMAGE_HEIGHT),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    val_transform = A.Compose(
+        [
+            A.Resize(width=IMAGE_WIDTH, height=IMAGE_HEIGHT),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    model = UNET(in_channels=3, out_channels=1).to(device=DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()  # Binary Cross Entropy : Binary Segmentation이므로.
+    # WithLogitsLoss는 우리가 model output에 sigmoid를 안해줬기 때문
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+        val_transform,
+        NUM_WORKERS,
+        PIN_MEMORY,
+    )
+
+    scaler = torch.cuda.amp.GradScaler()
+    for epoch in range(NUM_EPOCHS):
+        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+        # save checkpoint
+        # check accuracy
+        # print some examples to a directory
+
+
+if __name__ == "__main__":
+    main()
