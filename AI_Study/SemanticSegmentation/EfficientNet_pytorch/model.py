0b09081f58095e464bdae9c29f38851f0b44f6d5
KJYoung <jykim157@snu.ac.kr>
2023-09-25 20:01:47 +0900

    [AI_EfficientNet] 02_CNNBlock, Squeeze-and-Excitation

--- AI_Study/SemanticSegmentation/EfficientNet_pytorch/model.py
+++ AI_Study/SemanticSegmentation/EfficientNet_pytorch/model.py
@@ -17,9 +17,9 @@ base_model = [
 # alpha(depth, 1.2), beta(width, 1.1), gamma(resolution, 1.15)
 phi_values = {
     # tuple of (phi_value, resolution, drop_rate)
-    'b0': (0, 224, 0.2),
-    'b1': (0.5, 240, 0.2),
-    'b2': (1, 260, 0.3),
+    'b0': (0, 224, 0.2),  # Baseline => [phi = 0]
+    'b1': (0.5, 240, 0.2),  # ex. 224 * (1.15)^(0.5) = 240
+    'b2': (1, 260, 0.3),  # ex. 224 * (1.15) = 257.6
     'b3': (2, 300, 0.3),
     'b4': (3, 380, 0.4),
     'b5': (4, 456, 0.4),
@@ -29,15 +29,48 @@ phi_values = {
 
 
 class CNNBlock(nn.Module):
-    pass
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups=1):
+        super(CNNBlock, self).__init__()
+        self.cnn = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            groups=groups,  # depth-wise conv를 하기 위해서 groups 옵션을 둬야 함.
+        )
+        # groups = 1 : Normal Conv
+        # groups = in_channels : Depthwise Conv.
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.silu = nn.SiLU()
+
+    def forward(self, x):
+        return self.silu(self.bn(self.cnn(x)))
 
 
 class SqueezeExcitation(nn.Module):
-    pass
+    def __init__(self, in_channels, reduced_dim):
+        super(SqueezeExcitation, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),  # C * H * W => C * 1 * 1
+            nn.Conv2d(in_channels=in_channels, out_channels=reduced_dim, kernel_size=1),
+            nn.SiLU(),
+            nn.Conv2d(reduced_dim, in_channels, 1),  # resulting shape: C * 1 * 1
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.se(x)
 
 
+# MBConv
 class InvertedResidualBlock(nn.Module):
-    pass
+    def __init__(self):
+        super(InvertedResidualBlock, self).__init__()
+        pass
+
+    def forward(self, x):
+        pass
 
 
 class EfficientNet(nn.Module):
