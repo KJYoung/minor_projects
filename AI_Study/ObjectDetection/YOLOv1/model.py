6582cdc695f4641beedc3389927a62e98f80607f
KJYoung <jykim157@snu.ac.kr>
2023-09-26 22:52:36 +0900

    [AI_YOLOv1] 01_Skeleton, Architecture Configs

---
+++ AI_Study/ObjectDetection/YOLOv1/model.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+
+architecture_config = [
+    # Kernel-size, Out_channel, Stride, Padding
+    (7, 64, 2, 3),
+    "M",
+    (3, 192, 1, 1),
+    "M",
+    (1, 128, 1, 0),
+    (3, 256, 1, 1),
+    (1, 256, 1, 0),
+    (3, 512, 1, 1),
+    "M",
+    # [Tuples], Repeats
+    [(1, 256, 1, 0), (3, 512, 1, 1), 4],
+    (1, 512, 1, 0),
+    (3, 1024, 1, 1),
+    "M",
+    [(1, 512, 1, 0), (3, 1024, 1, 1), 2],
+    (3, 1024, 1, 1),
+    (3, 1024, 2, 1),
+    (3, 1024, 1, 1),
+    (3, 1024, 1, 1),
+]
+
+
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(CNNBlock, self).__init__()
+        self.conv = nn.Conv2D(in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.leaky_relu = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        return self.leaky_relu(self.bn(self.conv(x)))
+
+
+class YoloV1(nn.Module):
+    def __init__(self, in_channels=3, **kwargs):
+        super(YoloV1, self).__init__()
+        self.architecture = architecture_config
+        self.in_channels = in_channels
+        self.darknet = self._create_conv_layers(self.architecture)
+        self.fcs = self._create_fcs(**kwargs)
+
+    def _create_conv_layers(self):
+        pass
+
+    def _create_fcs(self):
+        pass
+
+    def forward(self, x):
+        x = self.darknet(x)
+        return self.fcs(torch.flatten(x, start_dim=1))
