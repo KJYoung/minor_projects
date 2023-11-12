e959bbc43c3d8dd115fa5872918cbffee935491a
KJYoung <jykim157@snu.ac.kr>
2023-11-12 21:08:28 +0900

    [LivingSolo|FEAT|FE] TrxnExcelDialog, XLSX Col Selector v1

--- LivingSolo/frontend/src/components/Trxn/TrxnExcelDialog.tsx
+++ LivingSolo/frontend/src/components/Trxn/TrxnExcelDialog.tsx
@@ -12,60 +12,75 @@ interface TrxnExcelDialogProps {
 };
 
 export const TrxnExcelDialog = ({open, handleClose, workbook} : TrxnExcelDialogProps) => {
-    const workbookSheetNames = workbook.SheetNames;
-    const [sheetSelect, setSheetSelect] = useState<string>(DEFAULT_OPTION);
-    const SheetContent = () => {
-        if(sheetSelect !== DEFAULT_OPTION){
-            const parsedSheet = xlsxParser(workbook.Sheets[sheetSelect]);
-            if(parsedSheet.length > 0){
-                const keys = Object.keys(parsedSheet[0] as any);
-                console.log(parsedSheet);
-                console.log(keys);
-                return <>
-                    {parsedSheet.map((row: any) => {
-                        return <>
-                            {keys.map((key) => {
-                                return <span key={key}>{row[key] && row[key]}, </span>
-                            })}
-                            <br />
-                        </>
-                    })}
-                </> 
-            }
+  const workbookSheetNames = workbook.SheetNames;
+  const [sheetSelect, setSheetSelect] = useState<string>(DEFAULT_OPTION);
+  const SheetContent = () => {
+    const parsedSheet = xlsxParser(workbook.Sheets[sheetSelect]);
+    const keys_ = Object.keys(parsedSheet[0] as any);
+    let [keys, setKeys] = useState<string[]>([...keys_]);
+    if(sheetSelect !== DEFAULT_OPTION){
+        if(parsedSheet.length > 0){
+            console.log(parsedSheet);
+            console.log(keys);
+            return <>
+              <ColNameDiv>
+                {keys_.map((key) => <div key={key} onClick={() => {
+                  if(keys.some((k) => k === key)){
+                    setKeys(keys.filter((k) => key !== k));
+                  }else{
+                    setKeys(k => [...k, key]);
+                  }
+                }}>{key} </div>)}
+              </ColNameDiv>
+                {parsedSheet.map((row: any) => {
+                    return <>
+                        {keys.map((key) => {
+                            return <span key={key}>{row[key] && row[key]}, </span>
+                        })}
+                        <br />
+                    </>
+                })}
+            </> 
         }
-        return <></>
     }
-    return <div>
-      <BootstrapDialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-        fullWidth={true}
-        maxWidth={false}
-      >
-        <DialogBody>
-          <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
-            태그 설정
-            <select value={sheetSelect} onChange={(e) => setSheetSelect(e.target.value)}>
-                <option disabled value={DEFAULT_OPTION}>
-                    - 시트를 선택하세요. -
-                </option>
-                {workbookSheetNames.map((sheetName) => <option value={sheetName} key={sheetName}>{sheetName}</option>)}
-            </select>
-          </BootstrapDialogTitle>
-          <DialogContent dividers> 
-            {sheetSelect !== DEFAULT_OPTION && <SheetContent />}  
-          </DialogContent>
-          <DialogActions>
-            <Button autoFocus onClick={handleClose}>
-              닫기
-            </Button>
-          </DialogActions>
-        </DialogBody>
-      </BootstrapDialog>
-    </div>
+    return <></>
+  }
+  return <div>
+    <BootstrapDialog
+      onClose={handleClose}
+      aria-labelledby="customized-dialog-title"
+      open={open}
+      fullWidth={true}
+      maxWidth={false}
+    >
+      <DialogBody>
+        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+          태그 설정
+          <select value={sheetSelect} onChange={(e) => setSheetSelect(e.target.value)}>
+              <option disabled value={DEFAULT_OPTION}>
+                  - 시트를 선택하세요. -
+              </option>
+              {workbookSheetNames.map((sheetName) => <option value={sheetName} key={sheetName}>{sheetName}</option>)}
+          </select>
+        </BootstrapDialogTitle>
+        <DialogContent dividers> 
+          {sheetSelect !== DEFAULT_OPTION && <SheetContent />}  
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            닫기
+          </Button>
+        </DialogActions>
+      </DialogBody>
+    </BootstrapDialog>
+  </div>
 };
 
 const DialogBody = styled.div`
   width: 100%;
+`;
+
+const ColNameDiv = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
